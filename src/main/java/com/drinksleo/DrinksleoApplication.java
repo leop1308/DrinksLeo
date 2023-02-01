c810eeb3cea15ac65ab2e89c19fdb5026986debd
Leonardo Irwin Plaça <leop1308@gmail.com>
2023-02-01 11:12:52 -0300

    First Commit - Starting project DrinksLeo

---
+++ src/main/java/com/drinksleo/DrinksleoApplication.java
@@ -0,0 +1,13 @@
+package com.drinksleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksleoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drinksleo/drinksleo/DrinksleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinksleo.drinksleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksleoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
